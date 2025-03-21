fd98f81cf4a869c90dd82e59613af0ca43d48496
Jhohan Hernandez <jhernandez362@unab.edu.co>
2025-03-21 09:53:47 -0500

    resolve filter type error

--- api/getProductField.tsx
+++ api/getProductField.tsx
@@ -1,4 +1,5 @@
-import { ResultFiltersType } from "@/.next/types/filters";
+
+import { ResultFiltersType } from "@/.next/types/result-filters-type";
 import { useEffect, useState } from "react";
 
 export function useGetProductField(){
